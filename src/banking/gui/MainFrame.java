2fbdfe6a51f81d58d3e14367e3394ce716dd338a
cmbartee <cmbartee@asu.edu>
2016-02-19 16:18:45 -0700

    Issue #3 Fix

--- src/banking/gui/MainFrame.java
+++ src/banking/gui/MainFrame.java
@@ -53,7 +53,7 @@ class MainFrame extends JFrame {
 		nameLabel		= new JLabel(props.getProperty("NameLabel"));
 		balanceLabel	= new JLabel(props.getProperty("BalanceLabel"));
 
-		Object[] accountTypes = {"Savings", "Checking"};
+		String[] accountTypes = {"Savings", "Checking"};
 		typeOptions = new JComboBox(accountTypes);
 		nameField = new JTextField(20);
 		balanceField = new JTextField(20);
